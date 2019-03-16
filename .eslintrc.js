729a44f668be7eff829672f71efcf011c823a418
Глебка Фокин <g.fokin@n1.ru>
2019-03-16 16:48:55 +0700

    Добавляет примеры с кастомными правилами и с проксированием no-restricted-* правил

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,17 @@
 module.exports = {
-    extends: [
-        'airbnb-base',
+  // Подключим airbnb чтобы показать как можно решить конфликты одинаковых правил, объявленных в разных плагинах
+  extends: ["airbnb-base"],
+  plugins: ["local"],
+  rules: {
+    "no-restricted-imports": ["error", { patterns: ["moment"] }],
+    "local/no-restricted-imports": ["warn", { patterns: ["lodash"] }],
+    "local/no-restricted-syntax": [
+      "error",
+      {
+        selector: `IfStatement`,
+        message: `If - не модно, используй тернарник 💁‍`
+      }
     ],
-    plugins: [
-        'local'
-    ],
-}
+    "local/no-null": ["error"]
+  }
+};
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = 42;
+// no-restricted-import кидает ошибку
+import moment from 'moment';
+// При этом, local/no-restricted-import кидает предупреждение
+import _ from 'lodash';
+
+const items = [1, 2, 3];
+
+// no-restricted-syntax от airbnb кидает ошибку
+for (const item in items) {
+  moment(item);
+}
+
+// Тут уже местный local/no-restricted-syntax кидает ошибку
+if (Math.random() > 0.5) {}
+
+// Здесь ошибку кидает наш кастомный local/no-null
+_.noop({ a: null });
